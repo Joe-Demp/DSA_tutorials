7674a7edb2e28a39b15fc5cdb36e2ff9992073d3
Joe-Demp <dempseyjoseph96@yahoo.com>
2018-11-20 18:09:47 +0000

    Adding first draft of RussianPeasant

---
+++ src/RussianPeasant.java
@@ -0,0 +1,33 @@
+/**
+ * To implement the Russian Peasant's multiplication algorithm
+ * 	and then time it
+ * @author demps
+ *
+ */
+public class RussianPeasant {
+	/**
+	 * Multiplies two numbers together by the Russian Peasant's algorithm
+	 * 
+	 * @param a the first operand, ideally the smaller of the two
+	 * @param b the second operand, ideally the larger of the two
+	 * @return a * b as calculated by the algorithm 
+	 */
+	public static long multiply(long a, long b) {
+//		first make sure the numbers are in the right order
+		if (a > b) {
+			long tmp = a;
+			a = b;
+			b = tmp;
+		}
+		
+//		then do the algorithm, storing the intermediate results in a stack
+		
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
