f296bf5aeae24a77e645ef956f46002cdfc2211c
Joe-Demp <dempseyjoseph96@yahoo.com>
2018-11-22 00:30:26 +0000

    Created a basic abstract class to act as a template for FileCounters

---
+++ src/filecounter/FileCounter.java
@@ -0,0 +1,6 @@
+package filecounter;
+
+public abstract class FileCounter {
+	public static int countFiles(String directory) {return -1;}
+	public static int countFiles(String directory, String fileType) {return -1;}	
+}
