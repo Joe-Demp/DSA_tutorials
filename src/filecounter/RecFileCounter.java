99d26796dd9976845fb0e6c568f352cb35b0dcd8
Joe-Demp <dempseyjoseph96@yahoo.com>
2018-11-22 00:36:18 +0000

    Created an outline for the Recursive File Counter

---
+++ src/filecounter/RecFileCounter.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package filecounter;
+
+/**
+ * @author demps
+ *
+ */
+public class RecFileCounter extends FileCounter {
+	
+	/**
+	 * Counts all the files in a given directory
+	 * @param directory a string indicating a directory on you computer
+	 * @return the number of files in the given directory
+	 */
+	public static int countFiles(String directory) {
+		//	TODO fill in this method
+		return -1;
+	}
+	
+	/**
+	 * Counts all the files of a given type in a given directory
+	 * @param directory a string indicating a directory on you computer
+	 * @param fileType a string giving the extension of the file type you're looking for<br>
+	 * 	e.g. "*.java" for java source files
+	 * @return the number of files of type fileType in directory
+	 */
+	public static int countFiles(String directory, String fileType) {
+		//	TODO fill in this method
+		return -1;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
